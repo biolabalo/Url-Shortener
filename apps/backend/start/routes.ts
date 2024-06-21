1e6b11d56c248872240dc8fd70a4504424aa6ee5
balogun biola <balogunbiola101@gmail.com>
2024-06-21 08:47:05 +0100

    feature: (implement auth api and signup screen)

--- apps/backend/start/routes.ts
+++ apps/backend/start/routes.ts
@@ -8,9 +8,17 @@
 */
 
 import router from '@adonisjs/core/services/router'
+const UsersController = () => import('#controllers/users_controller')
+import { middleware } from './kernel.js'
 
 router.get('/', async () => {
   return {
     hello: 'world',
   }
 })
+
+router.post('/register', [ UsersController, 'register']).as('auth.register')
+router.post('/login', [ UsersController, 'login']).as('auth.login')
+router.delete('/logout', [ UsersController, 'logout']).as('auth.logout').use(middleware.auth())
+router.get('/me', [ UsersController, 'me']).as('auth.me')
+router.get('/:shortUrl', 'UrlController.redirect')
